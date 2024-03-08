0bfbb9432d057abd80b6401cbc5e25b17a167a52
threeWindows <thec76ure@gmail.com>
2024-03-08 19:08:29 +0100

    add files

--- src/popup.ts
+++ src/popup.ts
@@ -10,23 +10,29 @@ import './popup.css';
   // To get storage access, we have to mention it in `permissions` property of manifest.json file
   // More information on Permissions can we found at
   // https://developer.chrome.com/extensions/declare_permissions
-  
+
   function main() {
     const title = document.getElementById('title') as HTMLInputElement;
     const text = document.getElementById('text') as HTMLInputElement;
     const save = document.getElementById('save');
 
     save?.addEventListener('click', () => {
-      chrome.storage.local.set({title: title?.value, content: text?.value}).then(() =>{ 
-        console.log('Value is set');
-      })     
+      chrome.storage.local
+        .set({ title: title?.value, content: text?.value })
+        .then(() => {
+          console.log('Value is set');
+        });
 
-      chrome.storage.local.get(["title", "content"]).then((result) => {
+      chrome.storage.local.get(['title', 'content']).then((result) => {
         console.log(`Title: ${result.title}\nContent: ${result.content}`);
-      })
-    })
+      });
+    });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      let tab = tabs[0];
+      tab.id && chrome.tabs.sendMessage(tab.id, { message: 'quickNote' });
+    });
   }
 
   document.addEventListener('DOMContentLoaded', main); //restoreCounter;
-
 })();
